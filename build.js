196ff7b1361820e350aaa2b713b65447e7e6870d
liguangsong <863925367@qq.com>
2018-02-06 13:32:16 +0800

    修改build脚本

--- build.js
+++ build.js
@@ -1,21 +1,25 @@
 
-var path = process.argv[2];
+let path = process.argv[2];
 
-var exec = require('child_process').exec;
-
-var child = exec('npm run build', function(err, stdout, stderr) {
+let exec = require('child_process').exec;
+exec('npm install', function(err, stdout, stderr) {
   if (err) throw err;
-  console.log(stdout)
-  console.log("构建成功")
-  if(path){
-    exec('cp -rf ./dist/* ../2018', function(err, stdout, stderr) {
-      if (err) {
-        console.log(stderr);
-        throw err;
-      }
-      console.log('已将dist build到'+path)
-    });
-  }
- 
-});
+  console.log(stdout);
+  exec('npm run build', function(err, stdout, stderr) {
+    if (err) throw err;
+    console.log(stdout);
+    console.log("构建成功");
+    if(path){
+      exec('cp -rf ./dist/* ../2018', function(err, stdout, stderr) {
+        if (err) {
+          console.log(stderr);
+          throw err;
+        }
+        console.log('已将dist build到'+path)
+      });
+    }
+    
+  });
+})
+
 
