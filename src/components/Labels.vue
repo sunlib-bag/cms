f65bb69140ee27b415428cde47baf4be56f514b2
liguangsong <863925367@qq.com>
2018-04-25 13:50:34 +0800

    删除标签编辑

--- src/components/Labels.vue
+++ src/components/Labels.vue
@@ -23,7 +23,7 @@
         <el-table-column
           label="操作">
           <template slot-scope="scope">
-            <el-button type="success" size="small" @click="showEditLabel(scope.row)">编辑</el-button>
+            <!--<el-button type="success" size="small" @click="showEditLabel(scope.row)">编辑</el-button>-->
             <el-button type="text" size="small"  class="red" @click="deleteLabel(scope.row.objectId)">删除</el-button>
           </template>
 
